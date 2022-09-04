61f579c2adc2085e2d3e22a9301039133b7ed12a
skedwards88 <skedwards88@github.com>
2022-09-04 11:38:43 -0700

    rename repo to fitness

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ if (process.env.NODE_ENV !== "development" && "serviceWorker" in navigator) {
   const path =
     location.hostname === "localhost"
       ? "/service-worker.js"
-      : "/exercise/service-worker.js";
-  const scope = location.hostname === "localhost" ? "" : "/exercise/";
+      : "/fitness/service-worker.js";
+  const scope = location.hostname === "localhost" ? "" : "/fitness/";
   window.addEventListener("load", () => {
     navigator.serviceWorker
       .register(path, { scope: scope })
